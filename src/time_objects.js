c8941b2749da3654f3a98d5168cebe78b4f83250
Rostyslav Zatserkovnyi <zatserkovnyi.rostyslav@gmail.com>
2015-08-17 14:29:49 +0300

    Removed ECMAScript6 implementation (Traceur seems to break angular), refactored timer objects as angular controllers, added UI representation

--- src/time_objects.js
+++ src/time_objects.js
@@ -1,155 +1,157 @@
-/**
- * Represents a timer that counts the time from its creation.
- * @param {number} counter - represents the time from the timer's creation.
- * (note: is @param still a JSDoc standard for ECMAScript 6 classes?)
- * @param {number} speed - represents the interval at which to run the tick() function (in ms)
- * @param {object} startTime - the time at which the coundown has started
- * @param {object} ticker - the counter function's timeout handle
- */
-
-class Stopwatch {
-    constructor(speed) {
-        this.counter = 0;
-        this.speed = speed;
-    }
-
-    /**
-     * The main counter function.
-     * Uses a self-adjusting technique for precise time calculation
-     * @return nothing
-     */
-
-    tick() {
-        var diff = (new Date().getTime() - this.startTime) - this.counter;
-
-        this.counter += this.speed;
-
-        this.ticker = window.setTimeout(this.tick.bind(this), (this.speed - diff));
-    }
-
-    /**
-     * Function used to start the counter
-     * Note: setInterval isn't used as the counter function repeatedly uses setTimeout on itself
-     * while adjusting for possible issues caused by CPU cycles
-     * @return nothing
-     */
-
-    start() {
-        this.startTime = new Date().getTime();
-        this.ticker = window.setTimeout(this.tick.bind(this), this.speed);
-    }
-
-    /**
-     * Function used to pause the counter
-     * @return nothing
-     */
-
-    pause() {
-        window.clearTimeout(this.ticker);
-    }
-
-    /**
-     * Function used to unpause the counter
-     * @return nothing
-     */
-
-    unpause() {
-        this.ticker = window.setTimeout(this.tick.bind(this), this.speed);
-    }
-
-    /**
-     * Function used to restart the counter.
-     * Resets all fields with the exception of speed.
-     * @return nothing
-     */
-
-    restart() {
-        window.clearTimeout(this.ticker);
-        this.counter = 0;
-        this.start();
-    }
-}
+var app = angular.module('TimingApp', []);
 
 /**
- * Represents a countdown that accepts a duration parameter and repeatedly decreases it until it reaches zero.
- * @param {number} counter - represents the time from the timer's creation.
- * (note: is @param still a JSDoc standard for ECMAScript 6 classes?)
- * @param {number} duration - represents the duration of the timer, used to keep info on restart
- * @param {number} speed - represents the interval at which to run the tick() function (in ms)
- * @param {object} startTime - the time at which the coundown has started
- * @param {object} ticker - the counter function's timeout handle
- * (maybe add onReachesZero parameter that accepts a function handle?)
+ * Represents a stopwatch that counts down time from its creation
+ * @param {number} counter - The time from the object's creation
+ * @param {number} speed - The interval between tick function calls
+ * @param {number} increment - The number added to the counter each tick
+ * @param {boolean} started - The object's start state (influences UI buttons)
+ * @param {boolean} paused - The object's pause state (influences UI buttons)
  */
 
-class Timer {
-    constructor(speed, duration) {
-        this.counter = duration;
-        this.speed = speed;
-        this.duration = duration;
-    }
+app.controller('StopwatchController', function($scope, $timeout) {
+    "use strict";
+
+    $scope.counter = 0;
+    $scope.speed = 1;
+    $scope.increment = 1;
+    $scope.started = false;
+    $scope.paused = false;
 
     /**
-     * The main counter function.
-     * Uses a self-adjusting technique for precise time calculation
-     * @return nothing
+     * Called each tick. Repeatedly sets a timeout on itself.
      */
 
-    tick() {
-        if (this.counter <= 0)
+    $scope.onTick = function () {
+        $scope.counter += $scope.increment;
+        ticker = $timeout($scope.onTick, $scope.speed);
+    };
+
+    /**
+     * Promise to be set to a timeout on the tick function
+     */
+
+    var ticker;
+
+    /**
+     * Starts the stopwatch, setting the promise
+     */
+
+    $scope.start = function() {
+        ticker = $timeout($scope.onTick, $scope.speed);
+        $scope.started = true;
+    };
+
+    /**
+     * Pauses the stopwatch without resetting the counter
+     */
+
+    $scope.pause = function () {
+        $timeout.cancel(ticker);
+        $scope.paused = true;
+    };
+
+    /**
+     * Unpauses the stopwatch if it's paused
+     */
+
+    $scope.unpause = function () {
+        $timeout.cancel(ticker);
+        $scope.paused = false;
+        ticker = $timeout($scope.onTick, $scope.speed);
+    };
+
+    /**
+     * Restarts the stopwatch, resetting the counter
+     */
+
+    $scope.restart = function () {
+        $timeout.cancel(ticker);
+        $scope.counter = 0;
+        $scope.paused = false;
+        ticker = $timeout($scope.onTick, $scope.speed);
+    }
+});
+
+/**
+ * Represents a timer that accepts a value and decrements it until it reaches zero
+ * @param {number} countdown - The initial value to be decremented
+ * @param {number} counter - The time from the object's creation
+ * @param {number} speed - The interval between tick function calls
+ * @param {number} increment - The number substracted from the counter each tick
+ * @param {boolean} started - The object's start state (influences UI buttons)
+ * @param {boolean} paused - The object's pause state (influences UI buttons)
+ */
+
+app.controller('TimerController', function($scope, $timeout) {
+    "use strict";
+
+    $scope.countdown = 1000;
+    $scope.counter = $scope.countdown;
+    $scope.speed = 1;
+    $scope.decrement = 1;
+    $scope.paused = false;
+    $scope.started = false;
+
+    /**
+     * Called each tick. Repeatedly sets a timeout on itself (until the counter reaches zero)
+     */
+
+    $scope.onTick = function()
+    {
+        if ($scope.counter <= 0)
         {
-            this.counter = 0;
-            window.clearTimeout(this.ticker);
+            $timeout.cancel(ticker);
         }
         else
         {
-            var diff = (new Date().getTime() - this.startTime) - (this.duration - this.counter);
-            this.counter -= this.speed;
-            this.ticker = window.setTimeout(this.tick.bind(this), (this.speed - diff));
+            $scope.counter -= $scope.decrement;
+            ticker = $timeout($scope.onTick, $scope.speed);
         }
-    }
+    };
 
     /**
-     * Function used to start the counter
-     * Note: setInterval isn't used as the counter function repeatedly uses setTimeout on itself
-     * while adjusting for possible issues caused by CPU cycles
-     * @return nothing
+     * Promise initially set to a timeout on the tick function
      */
 
-    start() {
-        this.startTime = new Date().getTime();
-        this.ticker = window.setTimeout(this.tick.bind(this), this.speed);
-    }
+    var ticker;
 
     /**
-     * Function used to pause the counter
-     * @return nothing
+     * Starts the timer, setting the promise
      */
 
-    pause() {
-        window.clearTimeout(this.ticker);
-    }
+    $scope.start = function() {
+        ticker = $timeout($scope.onTick, $scope.speed);
+        $scope.started = true;
+    };
 
     /**
-     * Function used to unpause the counter
-     * @return nothing
+     * Pauses the timer without resetting the counter
      */
 
-    unpause() {
-        this.ticker = window.setTimeout(this.tick.bind(this), this.speed);
-    }
+    $scope.pause = function () {
+        $timeout.cancel(ticker);
+        $scope.paused = true;
+    };
 
     /**
-     * Function used to restart the counter.
-     * Resets all fields with the exception of speed and duration.
-     * @return nothing
+     * Unpauses the timer if it's paused
      */
 
-    restart() {
-        window.clearTimeout(this.ticker);
-        this.counter = this.duration;
-        this.start();
-    }
-}
+    $scope.unpause = function () {
+        $timeout.cancel(ticker);
+        $scope.paused = false;
+        ticker = $timeout($scope.onTick, $scope.speed);
+    };
 
-var s = new Timer(50,10000);
-s.start();
+    /**
+     * Restarts the timer, resetting the counter to its initial value
+     */
+
+    $scope.restart = function () {
+        $timeout.cancel(ticker);
+        $scope.counter = $scope.countdown;
+        $scope.paused = false;
+        ticker = $timeout($scope.onTick, $scope.speed);
+    }
+});
